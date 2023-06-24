05e7a69a6c6b3dda9f2cf6222b86f10e53702f88
Orco <alexanderacosta2286@gmail.com>
2023-06-23 20:23:10 -0500

    basic setup

---
+++ models.py
@@ -0,0 +1,8 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True)
+
+    def __repr__(self):
+        return self.username
