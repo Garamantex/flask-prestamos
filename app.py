f0a71541f5cc09ccf4a9fc3fe3ecbaea732f3243
qdobyte <qdobyte@gmail.com>
2023-06-26 23:44:49 -0500

    all models

--- app.py
+++ app.py
@@ -11,85 +11,106 @@ migrate = Migrate(app, db)
 
 from models import User
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/menu-manager')
 def menu_manager():
     return render_template('menu-manager.html')
 
+
 @app.route('/menu-salesman')
 def menu_salesman():
     return render_template('menu-salesman.html')
 
+
 @app.route('/create-client')
-def createClient():
+def create_client():
     return render_template('create-client.html')
+
 
 @app.route('/renewal')
 def renewal():
     return render_template('renewal.html')
 
+
 @app.route('/box')
 def box():
     return render_template('box.html')
 
+
 @app.route('/create-box')
-def CreateBox():
+def create_box():
     return render_template('create-box.html')
 
+
 @app.route('/box-archive')
-def boxArchive():
+def box_archive():
     return render_template('box-archive.html')
 
+
 @app.route('/box-detail')
-def boxDetail():
+def box_detail():
     return render_template('box-detail.html')
 
+
 @app.route('/approval-expenses')
-def approvalAxpenses():
+def approval_expenses():
     return render_template('approval-expenses.html')
 
+
 @app.route('/morosos')
-def morosos():
-    return render_template('morosos.html')
+def debtor():
+    return render_template('debtor.html')
+
 
 @app.route('/wallet')
 def wallet():
     return render_template('wallet.html')
 
+
 @app.route('/wallet-detail')
-def walletDetail():
+def wallet_detail():
     return render_template('wallet-detail.html')
 
+
 @app.route('/credit-detail')
-def creditDetail():
+def credit_detail():
     return render_template('credit-detail.html')
 
+
 @app.route('/create-user')
-def createUser():
+def create_user():
     return render_template('create-user.html')
 
+
 @app.route('/user-list')
-def userList():
+def user_list():
     return render_template('user-list.html')
+
 
 @app.route('/reports')
 def reports():
     return render_template('reports.html')
 
+
 @app.route('/bills')
 def bills():
     return render_template('bills.html')
+
 
 @app.route('/income')
 def income():
     return render_template('income.html')
 
+
 @app.route('/outcome')
 def outcome():
     return render_template('outcome.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,8 +1,15 @@
+from enum import Enum
+
 from app import db
 import datetime
 
+
 # Definición de enumeración para el campo 'role' en User
-roles = db.Enum('administrador', 'coordinador', 'vendedor', 'cliente')
+# roles = db.Enum('Administrador', 'Coordinador', 'Vendedor')
+class Role(Enum):
+    ADMINISTRATOR = 1
+    COORDINATOR = 2
+    SELLER = 3
 
 
 class User(db.Model):
@@ -11,7 +18,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    role = db.Column(roles, nullable=False)
+    role = db.Column(db.Enum(Role), nullable=False, doc='Rol')
     first_name = db.Column(db.String(30), nullable=False, doc='Nombre')
     last_name = db.Column(db.String(30), nullable=False, doc='Apellido')
     cellphone = db.Column(db.String(20), nullable=False, doc='Celular')
@@ -73,21 +80,83 @@ class Salesman(db.Model):
         return f"{self.employee.user.first_name} {self.employee.user.last_name}"
 
 
+genders = db.Enum('Hombre', 'Mujer')
+
+
 class Client(db.Model):
     """ Modelo de Cliente """
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False, doc='Correo electrónico')
+    first_name = db.Column(db.String(30), nullable=False, doc='Nombre')
+    last_name = db.Column(db.String(30), nullable=False, doc='Apellido')
+    alias = db.Column(db.String(100), nullable=False, doc='Alias')
     document = db.Column(db.String(20), unique=True, nullable=False, doc='Documento')
+    gender = db.Column(genders, nullable=False, doc='Género')
+    cellphone = db.Column(db.String(20), nullable=False, doc='Celular')
     address = db.Column(db.String(100), nullable=False, doc='Dirección')
-    reference = db.Column(db.String(100), nullable=False, doc='Referencia')
+    neighborhood = db.Column(db.String(100), nullable=False, doc='Barrio')
+    status = db.bollean(default=True, nullable=False, doc='Estado')
+    debtor = db.bollean(default=False, nullable=False, doc='Deudor')
+    black_list = db.bollean(default=False, nullable=False, doc='Lista Negra')
     creation_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
     modification_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow,
                                   onupdate=datetime.datetime.utcnow)
 
-    user = db.relationship('User', backref='client', uselist=False)
     employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=True)
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
+
+
+class Loan(db.Model):
+    """ Modelo de Prestamo """
+
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Numeric(10, 2), nullable=False, doc='Monto')
+    dues = db.Column(db.Numeric(10, 2), nullable=False, doc='Cuotas')
+    interest = db.Column(db.Numeric(10, 2), nullable=False, doc='Interés')
+    payment = db.Column(db.Numeric(10, 2), nullable=False, doc='Pago')
+    status = db.bollean(default=True, nullable=False, doc='Estado')
+    up_to_date = db.bollean(default=False, nullable=False, doc='Al día')
+    creation_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    modification_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow,
+                                  onupdate=datetime.datetime.utcnow)
+
+    client_id = db.Column(db.Integer, db.ForeignKey('client.id'), nullable=False)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=False)
+
+    def __str__(self):
+        return f"{self.client.user.first_name} {self.client.user.last_name}"
+
+
+class TransactionType(Enum):
+    GASTO = 1,
+    INGRESO = 2,
+    RETIRO = 3
+
+
+class Concept(db.Model):
+    """ Modelo de Concepto """
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, doc='Nombre')
+    transaction_types = db.Column(db.Enum(TransactionType), nullable=False, doc='Tipo')
+
+
+class Transaction(db.Model):
+    """ Modelo de Transacción  """
+
+    id = db.Column(db.Integer, primary_key=True)
+    transaction_types = db.Column(db.Enum(TransactionType), nullable=False, doc='Tipo')
+    concept_id = db.Column(db.Integer, db.ForeignKey('concept.id'), nullable=False)
+    description = db.Column(db.String(100), nullable=False, doc='Descripción')
+    mount = db.Column(db.Numeric(10, 2), nullable=False, doc='Monto')
+    attachment = db.Column(db.String(100), nullable=True, doc='Adjunto')
+    status = db.bollean(default=False, nullable=False, doc='Estado')
+
+    concept = db.relationship('Concept', backref='transaction', uselist=False)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=False)
+
+    creation_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    modification_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow,
+                                  onupdate=datetime.datetime.utcnow)
