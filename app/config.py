78f565a45623ce51ea6cc5275b621ddcfea8df23
Andres Ramirez <qdobyte@gmail.com>
2023-12-19 23:19:29 -0500

    update config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 # Configuración de la base de datos
-SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost/rutzchile'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:toor1234@localhost/rutzchile'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Otras configuraciones
