e75301269599b1a38b380e5753e2559ead4fdb37
Alexander <alexanderacosta2286@gmail.com>
2024-03-26 08:38:59 -0500

    updating styles

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 # Configuración de la base de datos
-SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@localhost/rutzchile'
+SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost/rutzchile'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Otras configuraciones
