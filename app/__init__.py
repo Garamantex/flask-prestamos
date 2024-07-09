7d03aef9c4966d1b18b256018557d079ad78fcaf
Creative280 <alejandro.taborda280@gmail.com>
2024-07-08 21:09:53 -0500

    configuracion de bd hosting con el repositorio

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-def create_app():
+def create_app(use_hosting_db=False):
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
+    
+    # Elegir la base de datos a usar
+    if use_hosting_db:
+        app.config['SQLALCHEMY_DATABASE_URI'] = app.config['SQLALCHEMY_DATABASE_URI_HOSTING']
     
     # Aplicar opciones avanzadas del motor
     engine_options = app.config.get('SQLALCHEMY_ENGINE_OPTIONS', {})
@@ -15,7 +19,6 @@ def create_app():
     
     # Inicializar la instancia de SQLAlchemy
     db.init_app(app)
-
 
     # Configurar la migración de la base de datos
     from flask_migrate import Migrate
--- app/config.py
+++ app/config.py
@@ -4,6 +4,8 @@
 SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@localhost/rutzchile'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+# Configuración de la base de datos de hosting
+SQLALCHEMY_DATABASE_URI_HOSTING = 'mysql://prestchile13:3AHV4D0PS1s-@148.72.27.147/rutzchile'
 
 # Configuración avanzada de SQLAlchemy
 SQLALCHEMY_ENGINE_OPTIONS = {
