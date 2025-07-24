4124572d78ba87ef68b6850499e9eeb187c8f668
Creative280 <Alejandro.taborda280@gmail.com>
2025-07-24 18:38:01 -0500

    añadir filtro de plantilla para formatear valores monetarios y mejorar la lógica de gestión de cajas, incluyendo detalles de subadministradores y opciones de visualización de préstamos

--- app.py
+++ app.py
@@ -4,5 +4,13 @@ from app import create_app
 app = create_app()
 app.debug = True
 
+@app.template_filter('moneda_cl')
+def moneda_cl(value):
+    try:
+        value = int(value)
+        return "${:,.0f}".format(value).replace(",", ".")
+    except (ValueError, TypeError):
+        return value
+
 if __name__ == '__main__':
     app.run(debug=True)
